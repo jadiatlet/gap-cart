5f3b22d057c3dee7882b31d44d96faffdb0dde43
Panji Gumelar <panjiggm@gmail.com>
2018-12-28 16:37:35 +0700

    Create input cart to the table

--- cart/index.js
+++ cart/index.js
@@ -1,7 +1,8 @@
-const basicFood = document.querySelector('.items1')
-const clothes = document.querySelector('.items2')
-const stationary = document.querySelector('.items3')
+const foodOption = document.querySelector('.items1')
+const clothesOption = document.querySelector('.items2')
+const stationaryOption = document.querySelector('.items3')
 
+const form = document.getElementById('price-item')
 const priceOfItem = document.querySelector('.price-tag')
 const qtyItem = document.getElementById('qty-item')
 
@@ -9,27 +10,60 @@ const selectCatagory = document.getElementById('catagory').addEventListener('cha
   const catagoryItems = document.querySelector('.catagoryItems').value
 
   if (catagoryItems === 'Clothes') {
-    basicFood.style.display = 'none'
-    stationary.style.display = 'none'
-    clothes.style.display = 'block'
+    foodOption.style.display = 'none'
+    stationaryOption.style.display = 'none'
+    clothesOption.style.display = 'block'
   } else if (catagoryItems === 'Stationary') {
-    basicFood.style.display = 'none'
-    clothes.style.display = 'none'
-    stationary.style.display = 'block'
+    foodOption.style.display = 'none'
+    clothesOption.style.display = 'none'
+    stationaryOption.style.display = 'block'
   } else {
-    basicFood.style.display = 'block'
-    clothes.style.display = 'none'
-    stationary.style.display = 'none'
+    foodOption.style.display = 'block'
+    clothesOption.style.display = 'none'
+    stationaryOption.style.display = 'none'
   }
-
 })
 
-const food = document.getElementById('basic-food').addEventListener('change', () => {
-  const selectFood = basicFood.value
-
+const handleFood = () => {
+  const selectFood = foodOption.value
   priceOfItem.innerHTML = selectFood
+}
+
+const handleclothes = () => {
+  const selectClothes = clothesOption.value
+  priceOfItem.innerHTML = selectClothes
+}
+
+const handleStationary = () => {
+  const selectStationary = stationaryOption.value
+  priceOfItem.innerHTML = selectStationary
+}
+
+const food = document.getElementById('basic-food').addEventListener('change', handleFood)
+const clothes = document.getElementById('clothes').addEventListener('change', handleclothes)
+const stationary = document.getElementById('stationary').addEventListener('change', handleStationary)
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const selectFood = Number.parseInt(foodOption.value)
+  const selectClothes = Number.parseInt(clothesOption.value)
+  const selectStationary = Number.parseInt(stationaryOption.value)
+  const qty = qtyItem.value
+
+  const result = selectFood * qty
+
+  const itemList = document.getElementById('item-list')
+  const row = document.createElement('tr')
+
+  row.innerHTML = `
+    <td></td>
+    <td>${selectFood}</td>
+    <td>${qty}</td>
+    <td>${result}</td>
+  `
+
+  itemList.appendChild(row)
 })
 
 
-let a = qtyItem.value
-console.log(a);
